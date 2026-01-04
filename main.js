3d4723447280b7b8e649356e6c67744e2dbec605
AsadKhan1660 <Asad.Khan@ihorizons.com>
2026-01-04 01:52:01 +0000

    events and handling non blocking code

--- main.js
+++ main.js
@@ -19,15 +19,17 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       console.log(Buffer.concat(body).toString());
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
+      //fs.writeFileSync("message.txt", message); this is will block the server if we have big data
+      fs.writeFile("message.txt", message, () => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
