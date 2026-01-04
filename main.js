e33d75c3e9891f99de209672f2b20bfcc5e95d01
AsadKhan1660 <Asad.Khan@ihorizons.com>
2026-01-04 00:50:29 +0000

    initial commit

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const htmlContent = `<html><header>First Node Response</header>
 `;
 
 const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html");
   response.write(htmlContent);
+  response.end();
 });
 
 server.listen(3000);
