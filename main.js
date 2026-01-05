b24eb8e383a3dc3ad816439994028b35f6258b0c
AsadKhan1660 <Asad.Khan@ihorizons.com>
2026-01-05 19:41:18 +0000

    using next express instead of http

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const http = require("http");
-
+const express = require("express");
+const app = express();
 const routes = require("./routes");
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+  console.log("middle ware one");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middle ware two");
+  res.send(`<h1>hello from express js</h1>`);
+});
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
