5bfd0337a51a0c00c2a634c349c5f184252bc8c2
AsadKhan1660 <Asad.Khan@ihorizons.com>
2026-01-05 21:48:04 +0000

    creating styles and public for view

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 //const http = require("http");
-const bodyPaser = require("body-parser");
+const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 //const routes = require("./routes");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require("./util/path");
 
 app.use(
-  bodyPaser.urlencoded({
+  bodyParser.urlencoded({
     extended: false,
   })
 );
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>404 page not found</h1>`);
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 // const server = http.createServer(app);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,20 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get("/add-products", (req, res, next) => {
-  res.send(
-    `<form action="products" method="POST"><input type="text" name="product"><button type="submit">Send</button></form>`
-  );
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post("/products", (req, res, next) => {
-  console.log("body-parser", req.body);
-  res.redirect("/");
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
